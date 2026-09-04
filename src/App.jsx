3d743c66e436a3a8baa6f930fcb3244611a70799
shadab-ansari-aiits <shadabaits@gmail.com>
2026-09-04 17:41:28 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import Header from './components/Header/Header';
-import Hero from './components/Hero/Hero';
-import Services from './components/Services/Services';
-import About from './components/About/About';
-import WhyChooseUs from './components/WhyChooseUs/WhyChooseUs';
-import Contact from './components/Contact/Contact';
-import Footer from './components/Footer/Footer';
-import './App.css';
-import { FaPhoneAlt, FaWhatsapp } from 'react-icons/fa';
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Services from "./components/Services/Services";
+import About from "./components/About/About";
+import WhyChooseUs from "./components/WhyChooseUs/WhyChooseUs";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import "./App.css";
+import { FaPhoneAlt, FaWhatsapp } from "react-icons/fa";
 
 function App() {
-  const waUrl = "https://wa.me/918888678078?text=Hello%20PatelAirCondition,%20I%20want%20to%20book%20an%20AC%20service/repair.";
+  const waUrl =
+    "https://wa.me/918888678078?text=Hello%20PatelAirCondition,%20I%20want%20to%20book%20an%20AC%20service/repair.";
 
   return (
     <div className="app">
